59c7410071fe821f28f54bbe1d2426513edeea0e
Guillermo Ojeda <guiojeda@gmail.com>
2021-11-01 17:19:35 -0300

    Translated About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,27 +6,25 @@ export default function About() {
     return (
         <div>
             <div className={s.mainContainer} >
-                <h2>Acerca de este proyecto</h2>
-                <p>Este es un proyecto de muestra elaborado con Javascript y React. Funciona haciendo llamado a API del sitio web <a href="https://openweathermap.org/">OpenWeather </a>
-                    buscando datos climáticos y mostrándolos aquí.</p>
-                <h2>Instrucciones</h2>
-                <p>En la sección WeatherApp, coloque el nombre de una ciudad en el campo vacío de la parte superior
-                    derecha. Al presionar el botón "Agregar", se buscarán los datos climáticos actuales en OpenWeather.com
-                    y se mostrarán en la pantalla. Los datos de la ciudad se muestran en una tarjeta.Es posible mostrar
-                    datos de más de una ciudad a la vez.Para retirar los datos de una ciudad, presionar la "X" con fondo rojo
-                    en la tarjeta de la ciudad correspondiente.
+                <h2>About this project</h2>
+                <p>This is a sample project made with Javascript and React. It makes API calls to the site <a href="https://openweathermap.org/">OpenWeatherMap </a>
+                    to obtain weather data for a given city and showing it here.</p>
+                <h2>Instructions</h2>
+                <p>At the "WeatherApp" tab, type the name of a city of interest in the empty field at the top-right.
+                    When pressing "Add City", a small card will be shown in the screen containing the current weather
+                    data for that city, according to OpenWeatherMap.com. It is possible to show more than one card at
+                    a time. To remove a city card from view, press the "X" on the top-right corner of that card.
                 </p>
-                <h2>Acerca de mí</h2>
-                <p>Mi nombre es Guillermo Ojeda. Soy estudiante de programación Full-Stack en <a href="https://www.soyhenry.com/">SoyHenry</a> y actualmente
-                    estoy buscando dedicarme profesionalmente al rubro de tecnología. Disfruto mucho la programación web y la programación en general.También poseo
-                    conocimientos en Ciencia de Datos y Sistemas de Información Geográficos. Antes de dedicarme a la programación, trabajé en los rubros de agroindustrias
-                    y medio ambiente.</p>
-                <h2>Información de contacto: </h2>
+                <h2>About me</h2>
+                <p>I am Guillermo Ojeda. I recently fineished my Full-Stack course at <a href="https://www.soyhenry.com/">SoyHenry</a> and I am looking for a job
+                    in the field of technology. I enjoy web development a lot and programming in general.  I also have knowledges in Data Sciences and Geographic Information Systems.
+                    Before dedicating to programming, I worked in the field of agricuture industries and environmental engineering.</p>
+                <h2>Contact info: </h2>
                 <ul>
                     <li>Email: <a href="mailto:guiojeda@gmail.com">guiojeda @gmail.com</a></li>
                     <li>GitHub: <a href="https://github.com/guillermoojeda">https://github.com/guillermoojeda</a></li>
                     <li>LinkedIn: <a href="https://www.linkedin.com/in/guillermo-francsico-ojeda/">https://www.linkedin.com/in/guillermo-francsico-ojeda/</a></li>
-                    <li>Blog: <a href="https://guiojdata.wordpress.com/">https://guiojdata.wordpress.com/</a></li>
+                    <li>Data blog: <a href="https://guiojdata.wordpress.com/">https://guiojdata.wordpress.com/</a></li>
 
                 </ul>
             </div>
